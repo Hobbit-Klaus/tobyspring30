a27d4bf9c2f67d6dc2ca272107bd1281245272cf
Hobbit-Klaus <hwchon1234@naver.com>
2017-04-17 07:23:40 +0900

    apply spring IoC

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,14 +1,20 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by Hobbit-Klaus on 2017-04-17.
  */
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(getConnectionMaker());
     }
 
-    private ConnectionMaker getConnectionMaker() {
+    @Bean
+    public ConnectionMaker getConnectionMaker() {
         return new AnotherSimpleConnectionMaker();
     }
 }
