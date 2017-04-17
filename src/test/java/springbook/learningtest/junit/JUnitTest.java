788a224713a7fbf337efbe414b99b4c3a0fe3f63
Hobbit-Klaus <hwchon1234@naver.com>
2017-04-18 02:57:59 +0900

    add learning test JUnitTest

---
+++ src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -0,0 +1,34 @@
+package springbook.learningtest.junit;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Hobbit-Klaus on 2017-04-18.
+ */
+public class JUnitTest {
+    static Set<JUnitTest> testObjects = new HashSet<>();
+
+    @Test
+    public void test1() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test2() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test3() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+}
