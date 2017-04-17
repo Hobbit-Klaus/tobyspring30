90396e5b185e9a5ccd0fcb638bcca5d449ac7c06
Hobbit-Klaus <hwchon1234@naver.com>
2017-04-18 00:50:00 +0900

    replace main test to junit test

--- src/test/java/springbook/user/UserDaoTest.java
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -1,5 +1,9 @@
 package springbook.user;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.UserDao;
@@ -11,8 +15,9 @@ import java.sql.SQLException;
  * Created by Hobbit-Klaus on 2017-04-17.
  */
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        UserDao dao = new DaoFactory().userDao();
+
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
 //        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
@@ -24,20 +29,8 @@ public class UserDaoTest {
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
-
         User user2 = dao.get(user.getId());
-
-        if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        } else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        } else {
-            System.out.println("조회 테스트 성공");
-        }
-
-//        System.out.println(user2.getName());
-//        System.out.println(user2.getPassword());
-//        System.out.println(user2.getId() + " 조회 성공");
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
 }
