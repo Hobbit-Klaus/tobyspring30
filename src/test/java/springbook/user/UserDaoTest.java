b6415a1ea6a818db1f846de7113931955561902d
Hobbit-Klaus <hwchon1234@naver.com>
2017-04-18 00:56:40 +0900

    add method deleteAll And getCount

--- src/test/java/springbook/user/UserDaoTest.java
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -22,12 +22,16 @@ public class UserDaoTest {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
         user.setPassword("married");
 
         dao.add(user);
+        assertThat(dao.getCount(), is(1));
 
         User user2 = dao.get(user.getId());
         assertThat(user2.getName(), is(user.getName()));
