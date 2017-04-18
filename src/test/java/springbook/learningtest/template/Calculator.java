e6f564bf3a47f94fab79ac8afad9ea226abb4e9c
Hobbit-Klaus <hwchon1234@naver.com>
2017-04-19 06:30:51 +0900

    add learning test "template/callback"

---
+++ src/test/java/springbook/learningtest/template/Calculator.java
@@ -0,0 +1,35 @@
+package springbook.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by Hobbit-Klaus on 2017-04-19.
+ */
+public class Calculator {
+    public Integer calcSum(String filepath) throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer sum = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            return sum;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
